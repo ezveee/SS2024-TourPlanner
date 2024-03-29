8dee75a48dfb398a97d432033a60581fce4ff531
Sara Varga <saracloevarga@gmail.com>
2024-03-29 20:40:33 +0100

    Switched Business and DataAccess to Class Library from Console Application

---
+++ Business/Interfaces/ILogManager.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Models;
+
+namespace Business.Interfaces;
+internal interface ILogManager
+{
+	void CreateLog(DateTime date, string comment,
+		int difficulty, float distance,
+		double time, int rating, int tourId); // TODO: create struct for log details
+	void UpdateLog(int logId);
+	List<Tour> GetLogs(); // TODO: change Tour - it's a DAL Model, shouldn't be used in UI
+	void DeleteLog(int logId);
+}
---
+++ Business/Interfaces/ITourManager.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Models;
+
+namespace Business.Interfaces;
+public interface ITourManager
+{
+	void CreateTour(string name, string description,
+		string from, string to, string transportType,
+		float distance, double estimatedTime,
+		string routeInformation); // TODO: create strict for tour details
+	void UpdateTour(Tour modifiedTour);
+	List<Tour> GetTours();
+	void DeleteTour(Tour tour);
+}
