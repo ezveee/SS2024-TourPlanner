d72b16a4f153734f40c7a72ece60592cae0ce5ae
Sara Varga <saracloevarga@gmail.com>
2024-03-29 21:59:00 +0100

    Implemented LogManager [needs testing] still need to test if implementation works as it is supposed to implemented Create, Delete, GetAll and Update

--- Business/LogManager.cs
+++ Business/LogManager.cs
@@ -1,26 +1,76 @@
 ﻿using Business.Interfaces;
 using Business.Models;
+using DataAccess.Data;
+using DataAccess.Interfaces;
+using DataAccess.Repositories;
 
 namespace Business;
 public class LogManager : IManager<TourLog>
 {
+	private static readonly TourPlannerContext context = new();
+	private readonly IRepository<DataAccess.Models.TourLog> _repository = new TourLogRepository(context);
+
 	public void Create(TourLog entity)
 	{
-		throw new NotImplementedException();
+		_repository.Add(MapToDataAccess(entity));
 	}
 
 	public void Delete(TourLog entity)
 	{
-		throw new NotImplementedException();
+		_repository.Delete(MapToDataAccess(entity));
 	}
 
 	public List<TourLog> GetAll()
 	{
-		throw new NotImplementedException();
+		List<DataAccess.Models.TourLog> dataAccessLogs = _repository.GetAll().ToList();
+		List<TourLog> businessLogs = [];
+
+		foreach (DataAccess.Models.TourLog dataAccessLog in dataAccessLogs)
+		{
+			TourLog businessLog = MapToBusiness(dataAccessLog);
+			businessLogs.Add(businessLog);
+		}
+
+		return businessLogs;
 	}
 
 	public void Update(TourLog entity)
 	{
-		throw new NotImplementedException();
+		_repository.Update(MapToDataAccess(entity));
+	}
+
+	// TODO: ask if automapper is allowed
+	// if so -> should conf be in own mapping project?
+	// dependencies?
+	private DataAccess.Models.TourLog MapToDataAccess(TourLog log)
+	{
+		return new()
+		{
+			LogId = log.LogId,
+			Date = log.Date,
+			Comment = log.Comment,
+			Difficulty = log.Difficulty,
+			Distance = log.Distance,
+			Time = log.Time,
+			Rating = log.Rating,
+			TourId = log.TourId,
+
+			// TODO: Tour = get tour by id
+		};
+	}
+
+	private TourLog MapToBusiness(DataAccess.Models.TourLog log)
+	{
+		return new()
+		{
+			LogId = log.LogId,
+			Date = log.Date,
+			Comment = log.Comment,
+			Difficulty = log.Difficulty,
+			Distance = log.Distance,
+			Time = log.Time,
+			Rating = log.Rating,
+			TourId = log.TourId,
+		};
 	}
 }
