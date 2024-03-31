1369b7a01572024e16d315db8b0d5325abc96302
Sara Varga <saracloevarga@gmail.com>
2024-03-31 13:53:09 +0200

    Implemented TourManager Added Tour CRUD Operations for BLL and UI

---
+++ Business/Program.cs
@@ -0,0 +1,45 @@
+﻿using Business.Interfaces;
+using Business.Models;
+
+namespace Business;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Test");
+
+		IManager<Tour> manager = new TourManager();
+
+		// CREATE TEST
+		//Tour tour = new()
+		//{
+		//	Name = "CreateTest1",
+		//	Description = "Test",
+		//	From = "Test",
+		//	To = "Test",
+		//	TransportType = "Test",
+		//	Distance = 69.420f,
+		//	EstimatedTime = 6.9,
+		//	RouteInformation = "uwu"
+		//};
+		//manager.Create(tour);
+
+		// READ TEST 1
+
+		// READ TEST 2
+
+
+		// UPDATE TEST
+		Tour changedTour = manager.GetById(7);
+		if (changedTour is null)
+		{
+			throw
+		}
+		changedTour.Description = "Not 'Desc2' anymore [hopefully :')]";
+		changedTour.
+
+		// DELETE TEST
+
+	}
+}
--- Business/TourManager.cs
+++ Business/TourManager.cs
@@ -1,26 +1,83 @@
 ﻿using Business.Interfaces;
 using Business.Models;
+using DataAccess.Data;
+using DataAccess.Interfaces;
+using DataAccess.Repositories;
 
 namespace Business;
 public class TourManager : IManager<Tour>
 {
+	private static readonly TourPlannerContext context = new();
+	private readonly IRepository<DataAccess.Models.Tour> _repository = new TourRepository(context);
+
 	public void Create(Tour entity)
 	{
-		throw new NotImplementedException();
+		_repository.Add(MapToDataAccess(entity));
+	}
+
+	public List<Tour>? GetAll()
+	{
+		List<DataAccess.Models.Tour> dataAccessTours = _repository.GetAll().ToList();
+		List<Tour> businessLogs = [];
+
+		foreach (DataAccess.Models.Tour dataAccessTour in dataAccessTours)
+		{
+			Tour businessLog = MapToBusiness(dataAccessTour);
+			businessLogs.Add(businessLog);
+		}
+
+		return businessLogs;
+	}
+
+	public Tour? GetById(int id)
+	{
+		DataAccess.Models.Tour? tour = _repository.GetById(id);
+
+		return tour is not null ? MapToBusiness(tour) : null;
 	}
 
 	public void Delete(Tour entity)
 	{
-		throw new NotImplementedException();
-	}
-
-	public List<Tour> GetAll()
-	{
-		throw new NotImplementedException();
+		_repository.Delete(MapToDataAccess(entity));
 	}
 
 	public void Update(Tour entity)
 	{
-		throw new NotImplementedException();
+		_repository.Update(MapToDataAccess(entity));
+	}
+
+	// TODO: ask if automapper is allowed
+	// if so -> should conf be in own mapping project?
+	// dependencies?
+	private static DataAccess.Models.Tour MapToDataAccess(Tour tour)
+	{
+		return new()
+		{
+			TourId = tour.TourId,
+			Name = tour.Name,
+			Description = tour.Description,
+			From = tour.From,
+			To = tour.To,
+			TransportType = tour.TransportType,
+			Distance = tour.Distance,
+			EstimatedTime = tour.EstimatedTime,
+			RouteInformation = tour.RouteInformation
+		};
+	}
+
+	private static Tour MapToBusiness(DataAccess.Models.Tour tour)
+	{
+		return new()
+		{
+			TourId = tour.TourId,
+			Name = tour.Name,
+			Description = tour.Description,
+			From = tour.From,
+			To = tour.To,
+			TransportType = tour.TransportType,
+			Distance = tour.Distance,
+			EstimatedTime = tour.EstimatedTime,
+			RouteInformation = tour.RouteInformation
+		};
 	}
 }
