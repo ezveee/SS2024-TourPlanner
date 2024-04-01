97a3f2bb4d9a757189a6530734d8ba881a550f23
Sara Varga <saracloevarga@gmail.com>
2024-04-01 10:22:55 +0200

    Show TourLog based on on selected tour AGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH IT WORKSSSSSSSSSSSSSSSSSSSSSS AGHHHHHHHHHHHHHHHHHHHHHHH

--- UI/ViewModel/MainViewModel.cs
+++ UI/ViewModel/MainViewModel.cs
@@ -1,145 +1,146 @@
-﻿using Business;
-using Business.Interfaces;
-using Business.Models;
-using System;
-using System.Collections.Generic;
+﻿using Business.Models;
 using System.ComponentModel;
-using System.Drawing;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media.Imaging;
 using UI.Models;
 using UI.ViewModel.Commands;
 
-namespace UI.ViewModel
+namespace UI.ViewModel;
+
+internal class MainViewModel : INotifyPropertyChanged
 {
-    internal class MainViewModel : INotifyPropertyChanged
-    {
-        public ICommand OpenTourHandlerCommand { get; set; }
-        public ICommand OpenTourLogHandlerCommand { get; set; }
-        public ICommand SelectTourCommand { get; set; }
+	public ICommand OpenTourHandlerCommand { get; set; }
+	public ICommand OpenTourLogHandlerCommand { get; set; }
+	public ICommand SelectTourCommand { get; set; }
 
-        public MainViewModel()
-        {
-            OpenTourHandlerCommand = new RelayCommand(OpenTourHandlerWindow);
-            OpenTourLogHandlerCommand = new RelayCommand(OpenTourLogHandlerWindow);
-            SelectTourCommand = new RelayCommand(SelectTour);
+	public MainViewModel()
+	{
+		OpenTourHandlerCommand = new RelayCommand(OpenTourHandlerWindow);
+		OpenTourLogHandlerCommand = new RelayCommand(OpenTourLogHandlerWindow);
+		SelectTourCommand = new RelayCommand(SelectTour);
 
-            TourModel tourModel = new TourModel();
-            GetTourNames(tourModel);
+		TourModel tourModel = new();
+		GetTourNames(tourModel);
 
-            ImageModel imageModel = new ImageModel();
-            GetImage(imageModel);
-        }
+		ImageModel imageModel = new();
+		GetImage(imageModel);
+	}
 
-        #region Display Tours
-        private List<Tuple<int, string>> _tourNames;
-        public List<Tuple<int, string>> TourNames
-        {
-            get { return _tourNames ?? (_tourNames = new List<Tuple<int, string>>()); }
-            set
-            {
-                _tourNames = value;
-                OnPropertyChanged(nameof(TourNames));
-            }
-        }
+	#region Display Tours
+	private List<Tuple<int, string>> _tourNames;
+	public List<Tuple<int, string>> TourNames
+	{
+		get => _tourNames ??= [];
+		set
+		{
+			_tourNames = value;
+			OnPropertyChanged(nameof(TourNames));
+		}
+	}
 
-        private void GetTourNames(TourModel model)
-        {
-            _tourNames = new List<Tuple<int, string>>();
+	private Tuple<int, string> _selectedTour;
+	public Tuple<int, string> SelectedTour
+	{
+		get => _selectedTour;
+		set
+		{
+			_selectedTour = value;
+			OnPropertyChanged(nameof(SelectedTour));
+			SelectTour(value?.Item1);
+		}
+	}
 
-            foreach (var name in model.Tours)
-            {
-                Tuple<int, string> temp = new Tuple<int, string>(name.TourId, name.Name);
+	private void GetTourNames(TourModel model)
+	{
+		_tourNames = [];
 
-                _tourNames.Add(temp);
-            }
-        }
-        #endregion
+		foreach (Tour name in model.Tours)
+		{
+			Tuple<int, string> temp = new(name.TourId, name.Name);
 
-        #region Display Tourlogs
-        private List<TourLogModel> _tourLogs;
-        public List<TourLogModel> TourLogs
-        {
-            get { return _tourLogs ?? (_tourLogs = new List<TourLogModel>()); }
-            set
-            {
-                _tourLogs = value;
-                OnPropertyChanged(nameof(TourLogs));
-            }
-        }
-        private void SelectTour(object parameter)
-        {
-            GetTourLogData((int)parameter);
-        }
+			_tourNames.Add(temp);
+		}
+	}
+	#endregion
 
-        private void GetTourLogData(int id)
-        {
-            TourLogModel model = new TourLogModel();
+	#region Display Tourlogs
+	private List<TourLogModel> _tourLogs;
+	public List<TourLogModel> TourLogs
+	{
+		get => _tourLogs ??= [];
+		set
+		{
+			_tourLogs = value;
+			OnPropertyChanged(nameof(TourLogs));
+		}
+	}
+	private void SelectTour(object parameter)
+	{
+		GetTourLogData((int)parameter);
+	}
 
-            _tourLogs = new List<TourLogModel>();
+	private void GetTourLogData(int id)
+	{
+		TourLogModel model = new();
 
-            foreach (var item in model.TourLogs)
-            {
-                TourLogModel logModel = new TourLogModel()
-                {
-                    Date = item.Date,
-                    Comment = item.Comment,
-                    Difficulty = item.Difficulty,
-                    Distance = item.Distance,
-                    Time = item.Time,
-                    Rating = item.Rating
-                };
+		_tourLogs = [];
 
-                if(item.TourId == id)
-                {
-                    _tourLogs.Add(logModel);
-                }
-            }
-        }
-        #endregion
+		foreach (TourLog item in model.TourLogs)
+		{
+			TourLogModel logModel = new()
+			{
+				Date = item.Date,
+				Comment = item.Comment,
+				Difficulty = item.Difficulty,
+				Distance = item.Distance,
+				Time = item.Time,
+				Rating = item.Rating
+			};
 
-        #region Displaying Map Image (semi functional)
-        private string _image;
-        public string Image
-        {
-            get { return _image; }
-            set
-            {
-                _image = value;
-                OnPropertyChanged(nameof(Image));
-            }
-        }
+			if (item.TourId == id)
+			{
+				_tourLogs.Add(logModel);
+			}
+		}
 
-        private void GetImage(ImageModel imageModel)
-        {
-            _image = imageModel.img;
-        }
-        #endregion
+		OnPropertyChanged(nameof(TourLogs));
+	}
+	#endregion
+
+	#region Displaying Map Image (semi functional)
+	private string _image;
+	public string Image
+	{
+		get => _image;
+		set
+		{
+			_image = value;
+			OnPropertyChanged(nameof(Image));
+		}
+	}
+
+	private void GetImage(ImageModel imageModel)
+	{
+		_image = imageModel.img;
+	}
+	#endregion
 
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+	public event PropertyChangedEventHandler? PropertyChanged;
+	protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
 
-        private void OpenTourHandlerWindow(object parameter)
-        {
-            TourHandlerWindow tourHandlerWindow = new TourHandlerWindow();
-            tourHandlerWindow.Show();
-        }
+	private void OpenTourHandlerWindow(object parameter)
+	{
+		TourHandlerWindow tourHandlerWindow = new();
+		tourHandlerWindow.Show();
+	}
 
-        private void OpenTourLogHandlerWindow(object parameter)
-        {
-            TourLogHandlerWindow tourLogHandlerWindow = new TourLogHandlerWindow();
-            tourLogHandlerWindow.Show();
-        }
-    }
+	private void OpenTourLogHandlerWindow(object parameter)
+	{
+		TourLogHandlerWindow tourLogHandlerWindow = new();
+		tourLogHandlerWindow.Show();
+	}
 }
