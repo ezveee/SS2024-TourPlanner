4b3984ac585e4cd53a2140fc546d186dc33c7894
ineslelin <ines.l.lintner@gmail.com>
2024-05-30 18:03:57 +0200

    api call to ors directions works
    todo: parse results

--- Business/Api/OrsDirectionsApi.cs
+++ Business/Api/OrsDirectionsApi.cs
@@ -18,9 +18,12 @@ class Program
 		OrsDirectionsApi api = new OrsDirectionsApi();
 
 		await api.GeocodeGetCoordinatesAsync("Höchstädtplatz 6, 1200 Wien");
+
+		await api.DirectionsGetRouteAsync("Schwedenplatz, 1010 Wien", "Höchstädtplatz 6, 1200 Wien", "walking");
 	}
 }
 
+#region STRUCT DECLARATIONS
 public struct Coordinates
 {
 	public string lon;
@@ -41,6 +44,24 @@ public struct Root
 {
 	public Feature[] features;
 }
+#endregion
+
+// TODO: scuffed, change this future me pls pls plspls 
+public class TransportTypes
+{
+	public Dictionary<string, string> DTransportTypes = new Dictionary<string, string>()
+	{
+		{ "Walking",  "foot-walking" },
+		{ "walking", "foot-walking" },
+
+		{ "Car", "driving-car" },
+		{ "car", "driving-car" },
+
+		{ "Bike", "cycling-regular" },
+		{ "bike", "cycling-regular" }
+	};
+}
+
 
 public class OrsDirectionsApi
 {
@@ -74,9 +95,6 @@ public class OrsDirectionsApi
 				// LONGITUDE FIRST, LATITUDE SECOND
 				geocode.lon = data.features[0].geometry.coordinates[0];
 				geocode.lat = data.features[0].geometry.coordinates[1];
-
-				Console.WriteLine(geocode.lon);
-				Console.WriteLine(geocode.lat);
 			}
 		}
 
@@ -84,14 +102,32 @@ public class OrsDirectionsApi
 	}
 
 	// call directions api to receive distance, duration, etc
-	public Tour DirectionsGetRoute(string start, string end)
+	public async Task<Tour> DirectionsGetRouteAsync(string start, string end, string transportType)
 	{
 		Tour temp = new Tour();
+		TransportTypes transportTypes = new TransportTypes();
 
-		//Coordinates startPoint = GeocodeGetCoordinatesAsync(start);
-		//Coordinates endPoint = GeocodeGetCoordinatesAsync(end);
+		Coordinates startPoint = GeocodeGetCoordinatesAsync(start).Result;
+		Coordinates endPoint = GeocodeGetCoordinatesAsync(end).Result;
 
-		// call api here
+		using (_client = new HttpClient { BaseAddress = new Uri($"https://api.openrouteservice.org/v2/directions/{transportTypes.DTransportTypes[transportType]}" +
+																	$"?api_key={_apiKey}" +
+																	$"&start={startPoint.lon},{startPoint.lat}" +
+																	$"&end={endPoint.lon},{endPoint.lat}")})
+		{
+			_client.DefaultRequestHeaders.Clear();
+			_client.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8");
+
+			using (var response = await _client.GetAsync(""))
+			{
+				string responseData = await response.Content.ReadAsStringAsync();
+				var data = JsonConvert.DeserializeObject(responseData);
+
+				Console.WriteLine(data);
+
+				
+			}
+		}
 
 		return temp;
 	}
